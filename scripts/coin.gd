ce3eac5361d92d35035960bb4d914a9dce6a8743
carlosdiaz083-ux <carlos.diaz083@mymdc.net>
2026-03-31 01:17:12 -0400

    Fix mixed tabs/spaces indentation in coin.gd

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,10 +3,10 @@ extends Area2D
 # Coin/gem pickup - adds to player score
 @onready var game_manager: Node = %"Game Manager"
 
-  func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	self.get_node("Pickup Sound").play()
 	print("+1 coin")
 	game_manager.add_point()
 	self.visible = false
-  	await get_tree().create_timer(0.2).timeout
-  	self.queue_free()
+	await get_tree().create_timer(0.2).timeout
+	self.queue_free()
