9addaa371f9d5a82c8d60506dd28c79f011d7635
carlosdiaz083-ux <carlos.diaz083@mymdc.net>
2026-03-31 01:21:26 -0400

    Fix mixed tabs/spaces indentation in checkpoint_flag.gd

--- scripts/checkpoint_flag.gd
+++ scripts/checkpoint_flag.gd
@@ -2,22 +2,22 @@ extends Area2D
 
 # Checkpoint flag - saves player's spawn point when touched
 @onready var animated_sprite_2d: AnimatedSprite2D = $".."
-  @onready var game_manager: Node = $"../../../../../Game Manager"
+@onready var game_manager: Node = $"../../../../../Game Manager"
 
-  var is_active = false
+var is_active = false
 
-  func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	if (not is_active):
 		print("Checkpoint reached")
-      		animated_sprite_2d.play("waving")
-  		is_active = true
-  		game_manager.player_spawn.global_position = animated_sprite_2d.get_parent().global_position
-  		if Global.active_spawn == null:
+		animated_sprite_2d.play("waving")
+		is_active = true
+		game_manager.player_spawn.global_position = animated_sprite_2d.get_parent().global_position
+		if Global.active_spawn == null:
 			Global.active_spawn = animated_sprite_2d.get_node("Area2D")
-  		else:
+		else:
 			Global.active_spawn.checkpoint_deactivate()
-  			Global.active_spawn = animated_sprite_2d.get_node("Area2D")
+			Global.active_spawn = animated_sprite_2d.get_node("Area2D")
 
-  func checkpoint_deactivate():
+func checkpoint_deactivate():
 	self.get_parent().play("default")
-  	is_active = false
+	is_active = false
